cf39cb8921d7efccc35c2dbf5823962405d867b8
Ansykut <ansykut@gmail.com>
2023-09-28 16:07:12 -0400

    Port Constant changes

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@
 
 const http = require('http');
 
+const PORT = 3000;
+
 const server = http.createServer((req, res) => {
     res.end('Hello World!\n');
 });
 
-server.listen(4242, () => {
-    console.log('Server is running...');
+server.listen(PORT, () => {
+    console.log(`Server is listening on port http://localhost:${PORT}`);
 });
 

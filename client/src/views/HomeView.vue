ff574cdc57f3e1cf6fb5943e13e53502e58efe30
Ansykut <ansykut@gmail.com>
2023-10-05 16:01:21 -0400

    Caught up 10/5

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -3,7 +3,7 @@ import { ref } from 'vue';
 
 
 const newTask = ref('');
-const tasks = ref(<string[]>[]);
+const tasks = ref<string[]>([])
 
 function addTask() {
   tasks.value.push(newTask.value);
@@ -21,26 +21,22 @@ function addTask() {
       </p>
       <div class="panel-block">
         <p class="control has-icons-left">
-          <input class="input" type="text" placeholder="What do you want to do?">
+          <input class="input" type="text" placeholder="What do you want to do?"
+          @keypress.enter="addTask"
+          v-model="newTask">
           <span class="icon is-left">
             <i class="fas fa-plus" aria-hidden="true"></i>
           </span>
         </p>
       </div>
-      <p class="panel-tabs">
-        <a class="is-active">All</a>
-        <a>Public</a>
-        <a>Private</a>
-        <a>Sources</a>
-        <a>Forks</a>
-      </p>
       
-      <label class="panel-block">
+      <label class="panel-block" v-for="task in tasks">
         <input type="checkbox">
-        remember me
+        <span>{{ task }}</span>
       </label>
       <div class="panel-block">
-        <button class="button is-link is-outlined is-fullwidth">
+        <button class="button is-link is-outlined is-fullwidth"
+          @click = "tasks=[]">
           Reset all filters
         </button>
       </div>

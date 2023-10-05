988e832852e3e11ca54abda6d15f9e082556fede
Ansykut <ansykut@gmail.com>
2023-10-05 15:17:37 -0400

    End of class 10/5

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,11 +1,86 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+
+const newTask = ref('');
+const tasks = ref(<string[]>[]);
+
+function addTask() {
+  tasks.value.push(newTask.value);
+  newTask.value = '';
+};
 
 </script>
 
 <template>
   <main>
-    <h2>Home</h2>
-   
+    <h1>Home</h1>
+    <nav class="panel">
+      <p class="panel-heading">
+        To-Do
+      </p>
+      <div class="panel-block">
+        <p class="control has-icons-left">
+          <input class="input" type="text" placeholder="What do you want to do?">
+          <span class="icon is-left">
+            <i class="fas fa-plus" aria-hidden="true"></i>
+          </span>
+        </p>
+      </div>
+      <p class="panel-tabs">
+        <a class="is-active">All</a>
+        <a>Public</a>
+        <a>Private</a>
+        <a>Sources</a>
+        <a>Forks</a>
+      </p>
+      <a class="panel-block is-active">
+        <span class="panel-icon">
+          <i class="fas fa-book" aria-hidden="true"></i>
+        </span>
+        bulma
+      </a>
+      <a class="panel-block">
+        <span class="panel-icon">
+          <i class="fas fa-book" aria-hidden="true"></i>
+        </span>
+        marksheet
+      </a>
+      <a class="panel-block">
+        <span class="panel-icon">
+          <i class="fas fa-book" aria-hidden="true"></i>
+        </span>
+        minireset.css
+      </a>
+      <a class="panel-block">
+        <span class="panel-icon">
+          <i class="fas fa-book" aria-hidden="true"></i>
+        </span>
+        jgthms.github.io
+      </a>
+      <a class="panel-block">
+        <span class="panel-icon">
+          <i class="fas fa-code-branch" aria-hidden="true"></i>
+        </span>
+        daniellowtw/infboard
+      </a>
+      <a class="panel-block">
+        <span class="panel-icon">
+          <i class="fas fa-code-branch" aria-hidden="true"></i>
+        </span>
+        mojs
+      </a>
+      <label class="panel-block">
+        <input type="checkbox">
+        remember me
+      </label>
+      <div class="panel-block">
+        <button class="button is-link is-outlined is-fullwidth">
+          Reset all filters
+        </button>
+      </div>
+    </nav>
+
 
   </main>
 </template>
